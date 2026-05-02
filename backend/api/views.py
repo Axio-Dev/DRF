f9036768cc7921b6598ab22a435d2f20bbd2f92b
Axio Dev <axelvazmar0@gmail.com>
2026-05-01 22:29:52 -0600

    Trying to convert a Model instance into a dictionarie

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from django.http import JsonResponse, HttpResponse
 
 from products.models import Product
 
@@ -8,12 +9,13 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
         # this is a serialization and do the following things:
         # 1-. model instance (model_data)
         # 2-. turn a Python dict
         # 3-. Return JSON to my client
+        data = model_to_dict(model_data, fields=["id", "title", "price"])
     return JsonResponse(data)
+    #     print(data)
+    #     data = dict(data)
+    #     json_data_str = json.dumps(data)
+    # return JsonResponse(json_data_str, headers={"content_type": "application/json"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,6 +4,7 @@ endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"query": "Hello world!"}) # => This an API (Application Programming Interface) => Http requests
 
+# print(get_response.headers)
 # print(get_response.text) # Printing raw text response
 # print(get_response.status_code)
 print(get_response.json())
