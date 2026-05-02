abedcc4159382e069005dc0a04e3c62bdec8e8fe
Axio Dev <axelvazmar0@gmail.com>
2026-05-01 22:37:38 -0600

    Making our first rest API

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,18 @@
 import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse, HttpResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
-
+@api_view(["GET"]) # now, we need to declare what methods we will allowed on our API
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        # this is a serialization and do the following things:
-        # 1-. model instance (model_data)
-        # 2-. turn a Python dict
-        # 3-. Return JSON to my client
         data = model_to_dict(model_data, fields=["id", "title", "price"])
-    return JsonResponse(data)
-    #     print(data)
-    #     data = dict(data)
-    #     json_data_str = json.dumps(data)
-    # return JsonResponse(json_data_str, headers={"content_type": "application/json"})
+    return Response(data)
