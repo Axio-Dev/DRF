9a70c12fe10808c6a53e9d2a5db06a4a40ce3cba
Axio Dev <axelvazmar0@gmail.com>
2026-05-15 16:04:17 -0600

    Knowing about permissions and authentication

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductMixin.as_view()),
-    path('<int:pk>/', views.ProductMixin.as_view()),
+    path('', views.ProductListCreatAPIView.as_view()),
+    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
     path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
     path('<int:pk>/delete/', views.ProductDestroyAPIView.as_view()),
 ]
