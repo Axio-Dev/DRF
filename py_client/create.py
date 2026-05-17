9b8c46a03c2c074b6aa0bdea0565af7d7533b28c
Axio Dev <axelvazmar0@gmail.com>
2026-05-17 17:55:17 -0600

    Knowing about auth tokens

--- py_client/create.py
+++ py_client/create.py
@@ -2,11 +2,15 @@ import requests
 
 endpoint = "http://localhost:8000/api/products/"
 
+headers = {
+    'Authorization': 'Bearer 44bbf8ea855a4f0b506cea0806f9049596c54559'
+}
+
 
 data = {
     "title": "Oasis What's The Morning Story Vinil",
     "price": 51.99
 }
-get_response = requests.post(endpoint, json=data) # => This an API (Application Programming Interface) => Http requests
+get_response = requests.post(endpoint, json=data, headers=headers) # => This an API (Application Programming Interface) => Http requests
 
 print(get_response.json())
