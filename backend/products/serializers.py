7a3f92e03ccb39c006551314a5d1deb08adec844
Axio Dev <axelvazmar0@gmail.com>
2026-05-18 21:20:08 -0600

    Knowing about Update and Create in Model Serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,12 +1,22 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name="product-detail",
+        lookup_field = "pk"
+    )
+    email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
         fields = [
+            'url',
+            'edit_url',
+            'email',
             'pk',
             'title',
             'content',
@@ -14,6 +24,27 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount' # rendering the get_discount model method with other name
         ]
+
+    # def create(self, validated_data):
+    #     # return Product.objects.create(**validate_data)
+    #     # email = validated_data.pop("email")
+    #     obj = super().create(validated_data)
+    #     # print(email, obj)
+    #     return obj
+    
+    # def update(self, instance, validated_data):
+    #     email = validated_data.pop("email")
+    #     return super().update(instance, validated_data)
+    
+    def get_edit_url(self, obj):
+        """
+        Getting the entire url for edit a product
+        """
+        # return f"/api/v2/products/{obj.pk}/"
+        request = self.context.get("request")
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
     
     def get_my_discount(self, obj):
         """
